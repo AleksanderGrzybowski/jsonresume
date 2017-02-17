05f35e567a95d77cc8233bb3ce6ce0255f9f6451
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2017-02-17 13:12:35 +0100

    more

--- generate.js
+++ generate.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create();
 
-page.viewportSize = {width: 800, height: 768};
+page.viewportSize = {width: 800, height: 1131};
 page.open('file:///home/kelog/Kodzenie/jsonresume/resume.html', function () {
     page.render('out.pdf');
     phantom.exit();
